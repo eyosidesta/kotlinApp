4aeb11d9542f5720c39d7e4d6001e8df31926c38
eyosidesta <mamesmsdesta@gmail.com>
2024-07-15 04:30:22 -0400

    CAD Currency implementation added

--- app/src/main/java/com/example/kotlin_app/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_app/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.kotlin_app
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.*
@@ -8,43 +9,63 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var num1: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val num1:EditText = findViewById(R.id.edtnum1)
-        val num2:EditText = findViewById(R.id.edtnum2)
-        val btn_add:Button = findViewById(R.id.btn_add)
-        val btn_sub:Button = findViewById(R.id.btn_sub)
-        val btn_mul:Button = findViewById(R.id.btn_mul)
-        val btn_div:Button = findViewById(R.id.btn_div)
+        num1 = findViewById(R.id.edtnum1)
+        val btn_convert: Button = findViewById(R.id.btn_add)
         val rslt:TextView = findViewById(R.id.rslt)
+        val spinnerCurrencyConversion:Spinner = findViewById(R.id.spinnerId)
 
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currency_conversion,
+            android.R.layout.simple_spinner_item
+        ).also {adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinnerCurrencyConversion.adapter = adapter
+        }
 
+        spinnerCurrencyConversion.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>?, view: View, position: Int, id: Long) {
+                val currencyType = parent?.getItemAtPosition(position).toString()
+                updateHint(currencyType)
+            }
 
-        btn_add.setOnClickListener{view ->
-            var getNum1:Int = num1.text.toString().toInt()
-            var getNum2:Int = num2.text.toString().toInt()
-            var result = getNum1 + getNum2
-            rslt.text = "Result $result"
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+                // pass
+            }
         }
-        btn_sub.setOnClickListener{view ->
-            val getNum1:Int = num1.text.toString().toInt()
-            val getNum2:Int = num2.text.toString().toInt()
-            val result = getNum1 - getNum2
-            rslt.text = "Result $result"
+
+        btn_convert.setOnClickListener{view ->
+            val value = num1.text.toString().toDouble()
+            if (value != null) {
+                val currencyType = spinnerCurrencyConversion.selectedItem.toString()
+                val result = convert(value, currencyType)
+                val formatResult = String.format("%.2f", result)
+                rslt.text = "The Currency Conversion From CAD to $currencyType is $formatResult"
+            } else {
+                rslt.text = "Please enter a valid number"
+            }
         }
-        btn_mul.setOnClickListener{view ->
-            val getNum1:Int = num1.text.toString().toInt()
-            val getNum2:Int = num2.text.toString().toInt()
-            val result = getNum1 * getNum2
-            rslt.text = "Result $result"
+    }
+    private fun updateHint(currencyType: String) {
+        when (currencyType) {
+            "USD" -> num1.hint = "Enter Currency to USD"
+            "ETB" -> num1.hint = "Enter Currency to ETB"
+            "YUAN" -> num1.hint = "Enter Currency to YUAN"
         }
-        btn_div.setOnClickListener{view ->
-            val getNum1:Int = num1.text.toString().toInt()
-            val getNum2:Int = num2.text.toString().toInt()
-            val result = getNum1 / getNum2
-            rslt.text = "Result $result"
+    }
+
+    private fun convert(value: Double, currencyType: String): Double {
+        return when (currencyType) {
+            "USD" -> (value) * 0.73
+            "ETB" -> (value) * 42.19
+            "YUAN" -> (value) * 5.31
+            else -> value
         }
     }
 }
